fdbb47a2ddfc5c66d3a5cd529bfb11854d2ec8e2
Ionut <lungu.ioan98@gmail.com>
2017-03-03 17:16:55 +0200

    hello world server

---
+++ server/reactproject/src/main/java/com/lunguioan/server/ReactprojectApplication.java
@@ -0,0 +1,12 @@
+package com.lunguioan.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactprojectApplication.class, args);
+	}
+}
